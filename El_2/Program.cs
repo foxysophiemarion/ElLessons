2d9483f124c88c5532fee33d2845ad2d82229584
Gleb Rysev <botkot1991@gmail.com>
2023-05-21 00:01:35 +0400

    add El_2

---
+++ El_2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите номер дня недели (от 1 до 7):");
+int dayNumber = int.Parse(Console.ReadLine());
+
+switch (dayNumber)
+{
+	case 1:
+		Console.WriteLine("Понедельник");
+		break;
+	case 2:
+		Console.WriteLine("Вторник");
+		break;
+	case 3:
+		Console.WriteLine("Среда");
+		break;
+	case 4:
+		Console.WriteLine("Четверг");
+		break;
+	case 5:
+		Console.WriteLine("Пятница");
+		break;
+	case 6:
+		Console.WriteLine("Суббота");
+		break;
+	case 7:
+		Console.WriteLine("Воскресенье");
+		break;
+	default:
+		Console.WriteLine("Ошибка ввода. Введите число от 1 до 7.");
+		break;
+}
