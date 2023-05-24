15119e301f92ce2ede19d9d1e84ecbe42c3ade37
Gleb Rysev <botkot1991@gmail.com>
2023-05-24 22:38:21 +0400

    add El_3

---
+++ El_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа:");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+double squareRoot = Math.Sqrt(num2); // вычисляем корень квадратный из второго числа
+
+if ((squareRoot * squareRoot == num2) && (squareRoot == num1)) //сравниваем квадратный корень второго числа и первое число
+{
+	Console.WriteLine(num1 + " является квадратом числа " + num2);
+}
+else
+{
+	Console.WriteLine(num1 + " не является квадратом числа " + num2);
+}
