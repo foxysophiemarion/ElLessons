61fca3c152dfd650980dc32067eb960b72a5c244
Gleb Rysev <botkot1991@gmail.com>
2023-06-04 00:19:43 +0400

    add El_6

---
+++ El_6/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Сгенерированое число: " + number);
+
+void FindNewDigit(int num)
+{
+	int firstNumber = number / 100; // Деление нацело 78=7
+	int secondNumber = (number / 10) % 10;
+	int thirdNumber = number % 10;
+	int newNumber = firstNumber * 10 + thirdNumber;
+	Console.WriteLine("Вторая цифра числа: " + secondNumber);
+	Console.WriteLine("Число без второй цифры: " + newNumber);
+}
+
+FindNewDigit(number);
