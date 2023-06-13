73f7d59c7ec703081a70c77cfd0cebfc4a9aa32c
Gleb Rysev <botkot1991@gmail.com>
2023-06-13 23:13:36 +0400

    add El_8

---
+++ El_8/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int number = Prompt("Введите число: ");
+FindThirdNumber(number);
+int Prompt(string message)
+{
+	Console.Clear();
+	Console.WriteLine(message);
+	int num = int.Parse(Console.ReadLine());
+	return num;
+}
+
+void FindThirdNumber(int num)
+{
+	if (num < 100)
+	{
+		Console.WriteLine("Третьей цифры нет");
+	}
+	else
+	{
+		while (number >= 1000)
+		{
+			num = num / 10;
+		}
+		int thirdNumber = num % 10;
+		Console.WriteLine($"Третья цифра числа -> {thirdNumber}");
+	}
+}
