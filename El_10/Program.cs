b6ca56891b1bd7fec91eeb45c39535a752a810ac
Gleb Rysev <botkot1991@gmail.com>
2023-06-14 00:13:58 +0400

    add El_10

---
+++ El_10/Program.cs
@@ -0,0 +1,37 @@
+﻿int quarter = Prompt("Введите номер четверти: ");
+FindQuarter(quarter);
+
+int Prompt(string message)
+{
+	Console.Write(message);
+	int num = Convert.ToInt32(Console.ReadLine());
+	return num;
+}
+
+void FindQuarter(int num)
+{
+	if (num == 1)
+	{
+		Console.WriteLine("x>0, y>0");
+	}
+
+	if (num == 2)
+	{
+		Console.WriteLine("x<0, y>0");
+	}
+
+	if (num == 3)
+	{
+		Console.WriteLine("x<0, y<0");
+	}
+
+	if (num == 4)
+	{
+		Console.WriteLine("x>0, y<0");
+	}
+
+	else
+	{
+		Console.WriteLine("Недопустимый номер четверти.");
+	}
+}
