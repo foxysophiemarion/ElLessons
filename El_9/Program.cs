1f4a1e17d053d0f295bf491f6fe9a49b78878447
Gleb Rysev <botkot1991@gmail.com>
2023-06-13 23:38:30 +0400

    add El_9

---
+++ El_9/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int x1 = Prompt("Введите координату X: ");
+int y1 = Prompt("Введите координату Y: ");
+FindCoordinate(x1, y1);
+
+
+int Prompt(string message)
+{
+	Console.Write(message);
+	int num = int.Parse(Console.ReadLine());
+	return num;
+}
+
+void FindCoordinate(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		Console.WriteLine("Точка находится в первой четверти");
+	}
+
+	if (x < 0 && y > 0)
+	{
+		Console.WriteLine("Точка находится во второй четверти");
+	}
+
+	if (x < 0 && y < 0)
+	{
+		Console.WriteLine("Точка находится в третьей четверти");
+	}
+
+	if (x > 0 && y < 0)
+	{
+		Console.WriteLine("Точка находится в четвертой четверти");
+	}
+}
