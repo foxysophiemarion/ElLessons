b0897ecc020a475e142b2e93fb5092776e5b1fb9
Gleb Rysev <botkot1991@gmail.com>
2023-06-17 00:44:01 +0400

    add El_13

---
+++ El_13/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+string mess = "Введите число: ";
+int num = Prompt(mess);
+int summ = GetSumNums(num);
+
+Console.WriteLine($"Сумма = {summ}");
+
+int Prompt(string msg)
+{
+	Console.WriteLine(msg);
+	int number = int.Parse(Console.ReadLine());
+	return number;
+}
+
+
+
+//int sum = 0;
+//for (int i = 1; i <= num; i++)
+//{
+//	sum += i;
+//}
+
+//Console.WriteLine("Сумма чисел от 1 до {0} равна {1}", num, sum);
+
+int GetSumNums(int number)
+{
+	int sum = 0;
+	for (int i = 1; i <= number; i++)
+	{
+		sum += i;
+		Console.Write(sum + " ");
+	}
+	return sum;
+}
+
