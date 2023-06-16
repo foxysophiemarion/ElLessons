554c73e5603bb6673cbc694d22d97dbbef2343a2
Gleb Rysev <botkot1991@gmail.com>
2023-06-17 01:09:36 +0400

    add El_14

---
+++ El_14/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456-> 3
+//78-> 2
+//89126-> 5
+
+
+Console.Clear();
+
+int number = Prompt("Введите число: ");
+
+int Prompt(string msg)
+{
+	Console.WriteLine(msg);
+	int number = int.Parse(Console.ReadLine());
+	return number;
+}
+
+int count = 0;
+while (number != 0)
+{
+	count++;
+	number /= 10;
+
+}
+
+Console.WriteLine($"Количество цифр в числе: {count}");
