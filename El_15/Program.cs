e01280b268916e280f065c26201f8251887249aa
Gleb Rysev <botkot1991@gmail.com>
2023-06-17 01:52:50 +0400

    add El_15

---
+++ El_15/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+
+int Prompt(string msg)
+{
+	Console.WriteLine(msg);
+	int number = int.Parse(Console.ReadLine());
+	return number;
+}
+
+
+int Multipli(int number)
+{
+	int mult = 1;
+	for (int i = 1; i <= number; i++)
+	{
+		mult = mult * i;
+		Console.Write(mult + " ");
+	}
+	return mult;
+}
+
+int num = Prompt("Введите число: ");
+
+Console.WriteLine($"Произведение чисел: {Multipli(num)}");
